c5845dc7a6c813527279f18aeefc9a538cf31747
faranam <AradAryanBusiness@gmail.com>
2020-01-11 12:22:28 +0330

    Add project files.

---
+++ Write CSV to Excel/DataTableExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace Write_CSV_to_Excel
+{
+    public static class My_DataTable_Extensions
+    {
+        #region Exort To Excel
+        /// <summary>
+        /// Export DataTable into an excel file with field names in the header line
+        /// - Save excel file without ever making it visible if filepath is given
+        /// - Don't save excel file, just make it visible if no filepath is given
+        /// </summary>
+        /// <param name="tbl"></param>
+        /// <param name="excelFilePath"></param>
+        public static void ExportToExcel(this System.Data.DataTable tbl, string excelFilePath = null)
+        {
+            try
+            {
+                if (tbl == null || tbl.Columns.Count == 0)
+                    throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+                /// load excel, and create a new workbook
+                var excelApp = new Microsoft.Office.Interop.Excel.Application();
+                excelApp.Workbooks.Add();
+
+                /// single worksheet
+                Microsoft.Office.Interop.Excel._Worksheet workSheet = excelApp.ActiveSheet;
+
+                /// column headings
+                for (var i = 0; i < tbl.Columns.Count; i++)
+                {
+                    workSheet.Cells[1, i + 1] = tbl.Columns[i].ColumnName;
+                }
+
+                /// rows
+                for (var i = 0; i < tbl.Rows.Count; i++)
+                {
+                    /// to do: format datetime values before printing
+                    for (var j = 0; j < tbl.Columns.Count; j++)
+                    {
+                        workSheet.Cells[i + 2, j + 1] = tbl.Rows[i][j];
+                    }
+                }
+
+                /// check file path
+                if (!string.IsNullOrEmpty(excelFilePath))
+                {
+                    try
+                    {
+                        workSheet.SaveAs(excelFilePath);
+                        excelApp.Quit();
+                        Console.WriteLine("Jobs Done!");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
+                            + ex.Message);
+                    }
+                }
+                else
+                { /// no file path is given
+                    excelApp.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ExportToExcel: \n" + ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Write CSV to Excel/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace Write_CSV_to_Excel
+{
+    class Program
+    {
+        #region Fields
+        /// <summary>
+        /// Path of CSV File
+        /// </summary>
+        public static string pathCSV = @"C:\Users\faranam\Desktop\Exam\09 - Excel export\data.csv";
+
+        /// <summary>
+        /// output Excel File Path 
+        /// </summary>
+        public static string outputPath = @"C:\Users\faranam\Desktop\Exam\09 - Excel export\empty.xlsx";
+
+        /// <summary>
+        /// Data Table of CSV File
+        /// </summary>
+        public static System.Data.DataTable datatable = new System.Data.DataTable(tableName: "Excel Data");
+        #endregion
+
+        #region Read to File Function
+        /// <summary>
+        /// Read CSV file and Return Data as DataTable
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static System.Data.DataTable ReadFile(string path)
+        {
+            var rowsArray = File.ReadAllLines(path);
+            var rowArray = new string[6];
+            var column = new string[6];
+            column = rowsArray[0].Split(',');
+            foreach (var item in column)
+            {
+                datatable.Columns.Add(item);
+            }
+
+            for (int i = 1; i < rowsArray.Length; i++)
+            {
+                rowArray = rowsArray[i].Split(',');
+                DataRow row;
+                row = datatable.NewRow();
+                for (int j = 0; j < 6; j++)
+                {
+                    row[column[j]] = rowArray[j];
+                }
+                datatable.Rows.Add(row: row);
+            }
+
+            return datatable;
+        }
+        #endregion
+
+        #region Insert Data To Excel Function
+        /// <summary>
+        /// Write DataTable to Excel File
+        /// </summary>
+        /// <param name="input"></param>
+        public static void InsertDataToExcel(System.Data.DataTable input)
+        {
+            input.ExportToExcel(excelFilePath: outputPath);
+        }
+        #endregion
+
+        #region Main Function
+        /// <summary>
+        /// Main Function
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            InsertDataToExcel(ReadFile(pathCSV));
+
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
+
+
